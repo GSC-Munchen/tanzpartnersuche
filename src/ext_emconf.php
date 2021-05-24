2528dfd9c8ecc9dab99622a7d4d7af04d5b38b67
Peter v. Niebelschütz <peter.niebelschuetz@capgemini.com>
2021-05-24 17:03:31 +0200

    add an extension config file

---
+++ src/ext_emconf.php
@@ -0,0 +1,20 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Tanzpartnersuche',
+    'description' => 'This TYPO3 extension helps users find a dance partner.',
+    'category' => 'plugin',
+    'author' => 'GSC München',
+    'author_company' => 'GSC München',
+    'author_email' => 'ias@gsc-muenchen.de',
+    'state' => 'alpha',
+    'clearCacheOnLoad' => true,
+    'version' => '0.1-SNAPSHOT',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '10.4.16-10.99.99',
+        ]
+    ],
+    'autoload' => [
+    ],
+];
