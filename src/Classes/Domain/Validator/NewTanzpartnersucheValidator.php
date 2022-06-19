c8a5a1d1c59926384d715fbd8909b995340787ea
mn-gsc <84667612+mn-gsc@users.noreply.github.com>
2022-06-19 20:38:09 +0200

    extend validator for new profile

--- src/Classes/Domain/Validator/NewTanzpartnersucheValidator.php
+++ src/Classes/Domain/Validator/NewTanzpartnersucheValidator.php
@@ -24,8 +24,20 @@ class NewTanzpartnersucheValidator extends \TYPO3\CMS\Extbase\Validation\Validat
             $this->addError('Diese Mailadresse wurde bereits registriert. Bitte eine andere verwenden oder bestehenden Eintrag editieren/löschen. Ggfs. Passwort vergessen Funktion nutzen.', 1221563773);
         }
         
-        if ($newTanzpartnersuche->getPassword() == 'test') {
-            $this->addError('Schwaches Passwort.', 1262341709);
+        if ($newTanzpartnersuche->getGender() == '0') {
+            $this->addError('Bitte gib Dein Geschlecht an.', 1262341710);
+        }
+
+        if ($newTanzpartnersuche->getLevel() == '0') {
+            $this->addError('Bitte gib Dein Tanzniveau an.', 1262341711);
+        }
+
+        if ($newTanzpartnersuche->getCategory() == '0') {
+            $this->addError('Bitte gib Deine gewünschte Disziplin an.', 1262341712);
+        }
+
+        if ($newTanzpartnersuche->getRole() == '0') {
+            $this->addError('Bitte gib an, welche Rolle du suchst.', 1262341714);
         }
     }
 }
